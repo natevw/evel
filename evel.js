247a5a1ab19915440333f065bf3220e51afcee74
aaron <a.aa.akio.aaron@gmail.com>
2013-11-23 21:41:31 -0800

    Node.js support

--- evel.js
+++ evel.js
@@ -1,4 +1,6 @@
-var evel = function (code) {
+if (typeof exports === 'object') module.exports = evel; // node.js module support
+
+function evel(code) {
     if (typeof code !== 'string') return code;
     else if (code) return evel.Function("return ("+code+");")();
 };
